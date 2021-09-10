276d4818fe0003d6ae135e30caab67f61ee2aa88
daniel <danielkang09@naver.com>
2021-09-11 01:25:21 +0900

    blog-34 seperate header and footer make joinForm and loginForm

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BoardController {
 
     @GetMapping("/")
-    public String index()   {
+    public String index() {
 
         // /WEB-INF/views/index.jsp
         return "index";     // application.yml 에 spring 파트를 적어놨기 때문에, prifix로 앞에(/WEB-INF/views/), suffix로 뒤에(.jsp) 붙음
